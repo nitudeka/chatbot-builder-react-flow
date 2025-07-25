3f3fdca3d39ad7c0264041039d2575ddbf94fd19
nitudeka <iamnitudeka@gmail.com>
2025-07-25 19:28:04 +0530

    add active node UI

--- src/components/builder/nodes/index.tsx
+++ src/components/builder/nodes/index.tsx
@@ -2,22 +2,27 @@
   * Wrapper for all the node types
 */
 
+import { useMemo } from 'react';
 import type { NodeProps } from '@xyflow/react';
 import BuilderHandle from '../Handle';
 import { useConfigureNode } from '../contexts/ConfigureNodeContext';
 
 const Nodes: React.FC<{ data: any } & NodeProps> = (props) => {
-  const { setSelectedNode } = useConfigureNode()
+  const { selectedNode, setSelectedNode } = useConfigureNode()
 
   const onNodeClick = () => {
     setSelectedNode(props.data)
   }
 
+  const isNodeSelected = useMemo(() => {
+    return selectedNode?.id === props.data?.id
+  }, [selectedNode, props.data?.id])
+
   return (
     <div onClick={onNodeClick} className="cursor-pointer relative inline-block px-4 py-2 font-medium group">
-      <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
-      <span className="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
-      <div className="relative text-black group-hover:text-white">
+      <span className={[isNodeSelected ? "-translate-y-0 -translate-x-0" : "translate-x-1 translate-y-1", "absolute inset-0 w-full h-full transition duration-200 ease-out transform bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"].join(" ")}></span>
+      <span className={[isNodeSelected ? "bg-black" : "bg-white", "absolute inset-0 w-full h-full border-2 border-black group-hover:bg-black"].join(" ")}></span>
+      <div className={[isNodeSelected ? "text-white" : "text-black", "relative group-hover:text-white"].join(" ")}>
       <p className="text-sm">{props.data.label}</p>
       <BuilderHandle
 	type="target"
