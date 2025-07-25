8b48fd4dc2741471860e41977820ba94465aa778
nitudeka <iamnitudeka@gmail.com>
2025-07-25 16:00:01 +0530

    store react flow instance in context

--- src/components/builder/contexts/ConfigureNodeContext.tsx
+++ src/components/builder/contexts/ConfigureNodeContext.tsx
@@ -1,12 +1,14 @@
+import type { ReactFlowInstance } from '@xyflow/react';
 import { createContext, useContext, useState } from 'react';
  
 const ConfigureNodeContext = createContext([null, (_) => {}]);
  
 export const ConfigureNodeProvider: React.FC<{ children: React.ReactNode  }> = ({ children }) => {
   const [selectedNode, setSelectedNode] = useState<string | null>(null);
+  const [rfInstance, setRfInstance] = useState<ReactFlowInstance | null>(null);
  
   return (
-    <ConfigureNodeContext.Provider value={{selectedNode, setSelectedNode}}>
+    <ConfigureNodeContext.Provider value={{selectedNode, setSelectedNode, rfInstance, setRfInstance}}>
       {children}
     </ConfigureNodeContext.Provider>
   );
