3aac1b2560632732d8a99cd69bba7e8b5d80f8d3
nitudeka <iamnitudeka@gmail.com>
2025-07-25 11:36:53 +0530

    update sidedrawer component to handle close and remove backdrop

--- src/components/sideDrawer/index.tsx
+++ src/components/sideDrawer/index.tsx
@@ -3,8 +3,7 @@ const SideDrawer: React.FC<{ title: string; isOpen: boolean; onClose: () => void
 
   return (
     <div className="relative">
-      <div style={{ opacity: '0.7' }} onClick={props.onClose} className="fixed top-0 right-0 left-0 bottom-0 bg-black" />
-      <div className="flex flex-col justify-between w-72 bg-white h-full fixed top-0 right-0">
+      <div className="border-l flex flex-col justify-between w-72 bg-white h-full fixed top-0 right-0">
 	<div>
 	  <div className="px-2 font-semibold text-lg border-b">
 	    {props.title}
@@ -16,7 +15,7 @@ const SideDrawer: React.FC<{ title: string; isOpen: boolean; onClose: () => void
 	<div className="p-2 border-t">
 	  <div className="w-full cursor-pointer relative inline-block px-4 py-2 font-medium group">
 	    <span className="absolute inset-0 w-full h-full border-2 border-black bg-black"></span>
-	    <p className="relative text-center text-white">
+	    <p onClick={props.onClose} className="relative text-center text-white">
 	      Close
 	    </p>
 	  </div>
