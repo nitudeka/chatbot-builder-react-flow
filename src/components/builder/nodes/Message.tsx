221a9c95cc7c79ecc4d5ff896a4ac36aba0950d6
nitudeka <iamnitudeka@gmail.com>
2025-07-26 10:05:49 +0530

    add margin top in message node

--- src/components/builder/nodes/Message.tsx
+++ src/components/builder/nodes/Message.tsx
@@ -4,7 +4,7 @@ const NodeMessage: React.FC<Partial<NodeData>> = (props) => {
   if (!props.message) return null;
 
   return (
-    <div className="border-t max-w-xs">
+    <div className="border-t max-w-xs mt-1">
       <span className="text-xs text-gray-500">
 	{ props.message }
       </span>
